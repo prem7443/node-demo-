3dd39c20ba5f48a769adc3106cf3c33f33ce090b
prem7443 <123341381+prem7443@users.noreply.github.com>
2025-05-06 14:44:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end('Hello from Node.js\n');
+  res.end('Hello  Node.js\n');
 });
 
 server.listen(3000, () => {
